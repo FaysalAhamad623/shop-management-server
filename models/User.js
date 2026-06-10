6f6c7087a16560f59589bb987b14218394f8ec96
Md. Faysal Ahamad <faysalahamad2004@gmail.com>
2026-06-10 21:51:45 +0600

    complete user section

--- models/User.js
+++ models/User.js
@@ -48,4 +48,9 @@ userSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Match password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
+module.exports = mongoose.model('User', userSchema);
