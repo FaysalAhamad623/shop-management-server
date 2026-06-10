ad9c7b490b214561fad7a86139f7f46a273fd872
Md. Faysal Ahamad <faysalahamad2004@gmail.com>
2026-06-10 21:40:24 +0600

    update auth section

--- middleware/auth.js
+++ middleware/auth.js
@@ -45,3 +45,14 @@ exports.protect = async (req, res, next) => {
   }
 };
 
+// Admin only
+exports.admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({
+      success: false,
+      message: 'Not authorized as admin'
+    });
+  }
+};
