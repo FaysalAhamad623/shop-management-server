0fe877de6ce7ee54dcb7d494a7e4f5d048c679b2
Md. Faysal Ahamad <faysalahamad2004@gmail.com>
2026-06-10 21:42:38 +0600

    added product file

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a product name'],
+    trim: true,
+    maxlength: [100, 'Name cannot be more than 100 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [1000, 'Description cannot be more than 1000 characters']
+  },
+  
