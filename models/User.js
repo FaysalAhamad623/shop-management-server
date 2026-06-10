ecf1b3c598a16d8f28aaa80bec37435e89e34822
Md. Faysal Ahamad <faysalahamad2004@gmail.com>
2026-06-10 21:50:56 +0600

    update user with encript password before saving in user section

--- models/User.js
+++ models/User.js
@@ -38,3 +38,14 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// Encrypt password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+
